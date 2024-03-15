e40dee6b4a60af9b7dff496fbe3852366c2ff909
dmitrtynovik <cryinstone@gmail.com>
2024-03-15 12:02:14 +1100

    chore: refacotring DelayedRetry

--- src/main/java/DelayedRetry.java
+++ src/main/java/DelayedRetry.java
@@ -1,7 +1,10 @@
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DeliverCallback;
 
 public class DelayedRetry {
 
@@ -21,6 +24,29 @@ public class DelayedRetry {
             put("x-dead-letter-routing-key", Send.QUEUE_NAME);
             put("x-message-ttl", DELAY);
         }});
+    }
+
+    public static DeliverCallback createRetryDeliverCallback(Channel channel) {
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+            System.out.println(" [x] Received '" + message + "'");
+            if (delivery.getProperties().getHeaders() != null) {
+                ArrayList<Object> death =  (ArrayList<Object>) delivery.getProperties().getHeaders().get("x-death");
+                if (death != null) {
+                    HashMap<String,Object> first = (HashMap<String,Object>) death.get(0);
+    
+                    Long count = (Long) first.get("count");
+                    System.out.println("Retry number: " + count);
+                    if (count >= Recv.RETRIES) {
+                        channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+                        return;
+                    }
+                }
+            }
+    
+            channel.basicNack(delivery.getEnvelope().getDeliveryTag(), false, false);
+        };
+        return deliverCallback;
     }
     
 }
--- src/main/java/Recv.java
+++ src/main/java/Recv.java
@@ -1,17 +1,11 @@
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.DeliverCallback;
-import com.rabbitmq.client.Delivery;
-
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.HashMap;
 
 public class Recv {
 
     private final static String QUEUE_NAME = "delayed-retry";
-    private final static int RETRIES = 6;
+    final static int RETRIES = 6;
 
     public static void main(String[] argv) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
@@ -21,26 +15,9 @@ public class Recv {
 
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
-        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
-            System.out.println(" [x] Received '" + message + "'");
-            if (delivery.getProperties().getHeaders() != null) {
-                ArrayList<Object> death =  (ArrayList<Object>) delivery.getProperties().getHeaders().get("x-death");
-                if (death != null) {
-                    HashMap<String,Object> first = (HashMap<String,Object>) death.get(0);
-
-                    Long count = (Long) first.get("count");
-                    System.out.println("Retry number: " + count);
-                    if (count >= RETRIES) {
-                        channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
-                        return;
-                    }
-                }
-            }
-
-            channel.basicNack(delivery.getEnvelope().getDeliveryTag(), false, false);
-        };
-
-        channel.basicConsume(QUEUE_NAME, false, deliverCallback, consumerTag -> { });
+        channel.basicConsume(QUEUE_NAME, 
+            false, 
+            DelayedRetry.createRetryDeliverCallback(channel), 
+            consumerTag -> { });
     }
 }
